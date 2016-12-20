a71512aab4a7eb944fad13fb84e382f55b8233a0
Brady Wied <brady@wied.us>
2016-12-19 17:34:33 -0700

    Add import implementation

--- src/main/groovy/com/avioconsulting/b2b/maven/B2bImportMojo.groovy
+++ src/main/groovy/com/avioconsulting/b2b/maven/B2bImportMojo.groovy
@@ -12,5 +12,11 @@ class B2bImportMojo extends AbstractB2bMojo {
     private boolean overwrite
 
     void execute() throws MojoExecutionException, MojoFailureException {
+        def artifact = this.project.artifact.file
+        this.log.info "Importing B2B artifacts from ${artifact} to server..."
+        def antProject = createAntProject()
+        antProject.setProperty 'exportfile', artifact.absolutePath
+        antProject.setProperty 'overwrite', this.overwrite.toString()
+        runAntTarget antProject, 'b2bimport'
     }
 }

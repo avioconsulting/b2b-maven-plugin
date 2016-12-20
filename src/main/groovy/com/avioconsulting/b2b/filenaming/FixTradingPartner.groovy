732d292870afe8793736579aa5b9e295216714a4
Brady Wied <brady@wied.us>
2016-12-20 11:04:00 -0700

    Replace references in file

--- src/main/groovy/com/avioconsulting/b2b/filenaming/FixTradingPartner.groovy
+++ src/main/groovy/com/avioconsulting/b2b/filenaming/FixTradingPartner.groovy
@@ -1,6 +1,12 @@
 package com.avioconsulting.b2b.filenaming
 
 class FixTradingPartner {
+    private final Closure logger
+
+    FixTradingPartner(Closure logger) {
+        this.logger = logger
+    }
+
     /**
      * Fixes a single trading partner file and all references to it
      *
@@ -8,14 +14,32 @@ class FixTradingPartner {
      * @param directory - output directory containing all files
      * @return - the new ID of the trading partner
      */
-    static String fix(String tradingPartnerFile, File directory) {
+    String fix(String tradingPartnerFile, File directory) {
         def xmlFile = new File(directory, tradingPartnerFile)
         Node rootNode = new XmlParser().parse(xmlFile)
         def name = rootNode.@name
+        def oldId = rootNode.@id as String
         def newId = "tp_${name}"
         rootNode.@id = newId
+        def newFile = new File(directory, "${newId}.xml")
+        this.logger "Renaming ${xmlFile} to ${newFile} and updating ID..."
         new XmlNodePrinter(new IndentPrinter(new FileWriter(xmlFile))).print rootNode
-        xmlFile.renameTo(new File(directory, "${newId}.xml"))
+        xmlFile.renameTo(newFile)
+        new FileNameFinder().getFileNames(directory.absolutePath, '*.xml').each { otherFile ->
+            def otherFileObj = new File(otherFile)
+            def lines = otherFileObj.readLines()
+            def modified = false
+            def newLines = lines.collect { line ->
+                if (!modified && line.contains(oldId)) {
+                    modified = true
+                }
+                line.replaceAll(oldId, newId)
+            }
+            if (modified) {
+                this.logger "Fixing references found in ${otherFile}..."
+                otherFileObj.write(newLines.join(System.getProperty('line.separator')))
+            }
+        }
         name
     }
 }
--- src/test/groovy/com/avioconsulting/b2b/filenaming/FixTradingPartnerTest.groovy
+++ src/test/groovy/com/avioconsulting/b2b/filenaming/FixTradingPartnerTest.groovy
@@ -4,12 +4,15 @@ import org.apache.commons.io.FileUtils
 import org.junit.Before
 import org.junit.Test
 
+import static org.hamcrest.CoreMatchers.containsString
 import static org.hamcrest.Matchers.equalTo
 import static org.hamcrest.Matchers.is
+import static org.hamcrest.core.IsNot.not
 import static org.junit.Assert.assertThat
 
 class FixTradingPartnerTest {
     def directory = new File('build/tmp/sampleB2BFiles')
+    def logger = new FixTradingPartner({ str -> println "From class: ${str}" })
 
     @Before
     void setupFreshData() {
@@ -25,7 +28,7 @@ class FixTradingPartnerTest {
         def inputFilename = 'tp_luZRYV-7072537905454701067.xml'
 
         // act
-        def result = FixTradingPartner.fix inputFilename, directory
+        def result = logger.fix inputFilename, directory
 
         // assert
         assertThat result,
@@ -43,12 +46,35 @@ class FixTradingPartnerTest {
         def inputFilename = 'tp_luZRYV-7072537905454701067.xml'
 
         // act
-        FixTradingPartner.fix inputFilename, directory
+        logger.fix inputFilename, directory
         def expectedFile = new File(directory, 'tp_BradyInc.xml')
         def actualId = new XmlParser().parse(expectedFile).@id
 
         // assert
         assertThat actualId as String,
                    is(equalTo('tp_BradyInc'))
+    }
+
+    @Test
+    void references_othersChanged() {
+        // arrange
+        def inputFilename = 'tp_luZRYV-7072537905454701067.xml'
+
+        // act
+        logger.fix inputFilename, directory
+        def expectedRefFiles = [
+                'tpa_Gsf8462216507708172471.xml',
+                'tpa_vkKRnu-1313236717328621662.xml'
+        ]
+
+        // assert
+        expectedRefFiles.each { file ->
+            println "Checking file ${file}..."
+            def lines = new File(directory, file).readLines()
+            lines.each { line ->
+                assertThat line,
+                           not(containsString('tp_luZRYV-7072537905454701067'))
+            }
+        }
     }
 }

7ce69ef4d2c8ff0aedfb6eb73a43fd14bdb15776
Brady Wied <brady@wied.us>
2016-12-20 09:48:55 -0700

    Add purging of everything before importing/deploying

--- src/main/groovy/com/avioconsulting/b2b/maven/B2bImportMojo.groovy
+++ src/main/groovy/com/avioconsulting/b2b/maven/B2bImportMojo.groovy
@@ -4,12 +4,15 @@ import org.apache.maven.plugin.MojoExecutionException
 import org.apache.maven.plugin.MojoFailureException
 import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
+import org.apache.tools.ant.Project
 
 @SuppressWarnings("GroovyUnusedDeclaration")
 @Mojo(name = 'b2bImport')
 class B2bImportMojo extends AbstractB2bMojo {
     @Parameter(property = 'b2b.overwrite', defaultValue = 'true')
     private boolean overwrite
+    @Parameter(property = 'b2b.purgeBeforeImport', defaultValue = 'false')
+    private boolean purgeBeforeImport
 
     void execute() throws MojoExecutionException, MojoFailureException {
         def artifact = this.project.artifact.file
@@ -17,8 +20,29 @@ class B2bImportMojo extends AbstractB2bMojo {
         def antProject = createAntProject()
         antProject.setProperty 'exportfile', artifact.absolutePath
         antProject.setProperty 'overwrite', this.overwrite.toString()
+
+        if (this.purgeBeforeImport) {
+            doPurge antProject
+        }
+
         ['b2bimport', 'b2bvalidate', 'b2bdeploy'].each { target ->
             runAntTarget antProject, target
+        }
+    }
+
+    private void doPurge(Project antProject) {
+        this.log.info 'Purging B2B artifacts/data before importing...'
+        def runtimeArtifacts = 'RT'
+        def modes = [
+                runtimeArtifacts,
+                'DT'// design time artifacts
+        ]
+        antProject.setProperty 'archive', false.toString()
+        // want a full cleanup
+        antProject.setProperty 'purgecontrolnumber', true.toString()
+        modes.each { mode ->
+            antProject.setProperty 'mode', mode
+            runAntTarget antProject, 'b2bpurge'
         }
     }
 }

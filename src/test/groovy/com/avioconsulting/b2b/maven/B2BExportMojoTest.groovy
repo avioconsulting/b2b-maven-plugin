d8083afe3614e3b2196873d460935335c56198f7
Brady Wied <brady@wied.us>
2016-12-20 16:00:41 -0700

    Extract out shared code

--- src/test/groovy/com/avioconsulting/b2b/maven/B2BExportMojoTest.groovy
+++ src/test/groovy/com/avioconsulting/b2b/maven/B2BExportMojoTest.groovy
@@ -1,9 +1,6 @@
 package com.avioconsulting.b2b.maven
 
-import groovy.mock.interceptor.StubFor
 import groovy.test.GroovyAssert
-import org.apache.commons.io.FileUtils
-import org.apache.tools.ant.Project
 import org.junit.Before
 import org.junit.Test
 
@@ -11,23 +8,13 @@ import static org.hamcrest.Matchers.equalTo
 import static org.hamcrest.Matchers.is
 import static org.junit.Assert.assertThat
 
-class B2BExportMojoTest {
+class B2BExportMojoTest implements MojoAnt {
     B2bExportMojo mojo
-    def targetsRun
-    def propsUsed
-    final File baseDirectory = new File('build/tmp/the_base_dir')
-    final File zipFile = new File(baseDirectory, 'tmp/b2bExport.zip')
 
     @Before
     void setup() {
         mojo = new B2bExportMojo()
-        if (baseDirectory.exists()) {
-            baseDirectory.deleteDir()
-        }
-        baseDirectory.mkdirs()
         mojo.metaClass.getBaseDir = { -> baseDirectory }
-        targetsRun = []
-        propsUsed = [:]
     }
 
     @Test
@@ -52,7 +39,7 @@ class B2BExportMojoTest {
         // arrange
         mojo.doExport = true
         mojo.b2BArtifactType = B2BArtifactTypes.DocumentDefinitions
-        SimpleFileStub()
+        simpleFileStub()
 
         // act
         mojo.execute()
@@ -67,7 +54,7 @@ class B2BExportMojoTest {
         // arrange
         mojo.doExport = true
         mojo.b2BArtifactType = B2BArtifactTypes.DocumentDefinitions
-        SimpleFileStub()
+        simpleFileStub()
 
         // act
         mojo.execute()
@@ -82,7 +69,7 @@ class B2BExportMojoTest {
         // arrange
         mojo.doExport = true
         mojo.b2BArtifactType = B2BArtifactTypes.DocumentDefinitions
-        SimpleFileStub()
+        simpleFileStub()
 
         // act
         mojo.execute()
@@ -102,7 +89,7 @@ class B2BExportMojoTest {
         // arrange
         mojo.doExport = true
         mojo.b2BArtifactType = B2BArtifactTypes.PartnersAndAgreements
-        SimpleFileStub()
+        simpleFileStub()
 
         // act
         def exception = GroovyAssert.shouldFail {
@@ -121,7 +108,7 @@ class B2BExportMojoTest {
         mojo.b2BArtifactType = B2BArtifactTypes.PartnersAndAgreements
         mojo.partners = ['partner1']
         mojo.agreements = ['agree1']
-        SimpleFileStub()
+        simpleFileStub()
 
         // act
         mojo.execute()
@@ -136,62 +123,5 @@ class B2BExportMojoTest {
                    ]))
         assertThat new File(baseDirectory, 'src/main/resources/b2b/EDI_X12').exists(),
                    is(equalTo(false))
-    }
-
-    private createTradingPartnerFile(String name, File directory) {
-        def id = "tp_${name}"
-        def path = new File(directory, "${id}.xml")
-        path.write("<?xml version = '1.0' encoding = 'UTF-8'?>\n" +
-                           "<TradingPartner hosted=\"true\" id=\"${id}\" logoSO=\"/soa/b2b/seed/defaultSeededHostTpIcon.png\" name=\"${name}\" version=\"12.2.1.1.0\" xmlns=\"http://xmlns.oracle.com/integration/b2b/profile\"/>")
-    }
-
-    private createTradingPartnerAgreementFile(String name, File directory) {
-        def id = "tpa_${name}"
-        def path = new File(directory, "${id}.xml")
-        path.write("<?xml version='1.0' encoding='UTF-8'?>\n" +
-                           "<Agreement agreementId=\"${id}\" id=\"${id}\" name=\"${name}\" xmlns=\"http://xmlns.oracle.com/integration/b2b/profile\"/>")
-    }
-
-    private SimpleFileStub() {
-        getAntProjectMock(mojo) { t ->
-            def zipDir = new File(baseDirectory, 'b2bzipcontents')
-            def b2bDir = new File(zipDir, 'soa/b2b')
-            b2bDir.mkdirs()
-            def ediDir = new File(b2bDir, 'EDI_X12/v5010/837/837Default')
-            ediDir.mkdirs()
-            FileUtils.touch new File(b2bDir, 'doc_HL7.xml')
-            createTradingPartnerFile 'partner1', b2bDir
-            createTradingPartnerFile 'partner2', b2bDir
-            createTradingPartnerAgreementFile 'agree1', b2bDir
-            createTradingPartnerAgreementFile 'agree2', b2bDir
-            FileUtils.touch new File(ediDir, 'X12-5010-837.xsd')
-
-            def antBuilder = new AntBuilder()
-            antBuilder.zip(destFile: zipFile.absolutePath) {
-                fileset(dir: zipDir)
-            }
-            zipDir.deleteDir()
-        }
-    }
-
-    private getAntProjectMock(mojo, Closure<String> onTarget) {
-        def stub = new StubFor(Project)
-        def demand = stub.demand
-        demand.setProperty { prop, val ->
-            println "setting prop ${prop}"
-            propsUsed[prop] = val
-        }
-        demand.executeTarget { t ->
-            targetsRun << t
-            println "mock execute target ${t}"
-            onTarget(t)
-        }
-        demand.fireBuildStarted {}
-        demand.init {}
-        demand.fireBuildFinished { e -> }
-        mojo.metaClass.createAntProject = { ->
-            GroovySystem.getMetaClassRegistry().setMetaClass(stub.proxy.theClass, stub.proxy)
-            new Project()
-        }
     }
 }
---
+++ src/test/groovy/com/avioconsulting/b2b/maven/MojoAnt.groovy
@@ -0,0 +1,80 @@
+package com.avioconsulting.b2b.maven
+
+import groovy.mock.interceptor.StubFor
+import org.apache.commons.io.FileUtils
+import org.apache.tools.ant.Project
+import org.junit.Before
+
+trait MojoAnt {
+    def targetsRun
+    def propsUsed
+    final File baseDirectory = new File('build/tmp/the_base_dir')
+    final File zipFile = new File(baseDirectory, 'tmp/b2bExport.zip')
+
+    @Before
+    void mojoAntSetup() {
+        if (baseDirectory.exists()) {
+            baseDirectory.deleteDir()
+        }
+        baseDirectory.mkdirs()
+        targetsRun = []
+        propsUsed = [:]
+    }
+
+    def createTradingPartnerFile(String name, File directory) {
+        def id = "tp_${name}"
+        def path = new File(directory, "${id}.xml")
+        path.write("<?xml version = '1.0' encoding = 'UTF-8'?>\n" +
+                           "<TradingPartner hosted=\"true\" id=\"${id}\" logoSO=\"/soa/b2b/seed/defaultSeededHostTpIcon.png\" name=\"${name}\" version=\"12.2.1.1.0\" xmlns=\"http://xmlns.oracle.com/integration/b2b/profile\"/>")
+    }
+
+    def createTradingPartnerAgreementFile(String name, File directory) {
+        def id = "tpa_${name}"
+        def path = new File(directory, "${id}.xml")
+        path.write("<?xml version='1.0' encoding='UTF-8'?>\n" +
+                           "<Agreement agreementId=\"${id}\" id=\"${id}\" name=\"${name}\" xmlns=\"http://xmlns.oracle.com/integration/b2b/profile\"/>")
+    }
+
+    def simpleFileStub() {
+        getAntProjectMock(mojo) { t ->
+            def zipDir = new File(baseDirectory, 'b2bzipcontents')
+            def b2bDir = new File(zipDir, 'soa/b2b')
+            b2bDir.mkdirs()
+            def ediDir = new File(b2bDir, 'EDI_X12/v5010/837/837Default')
+            ediDir.mkdirs()
+            FileUtils.touch new File(b2bDir, 'doc_HL7.xml')
+            createTradingPartnerFile 'partner1', b2bDir
+            createTradingPartnerFile 'partner2', b2bDir
+            createTradingPartnerAgreementFile 'agree1', b2bDir
+            createTradingPartnerAgreementFile 'agree2', b2bDir
+            FileUtils.touch new File(ediDir, 'X12-5010-837.xsd')
+
+            def antBuilder = new AntBuilder()
+            antBuilder.zip(destFile: zipFile.absolutePath) {
+                fileset(dir: zipDir)
+            }
+            zipDir.deleteDir()
+        }
+    }
+
+    def getAntProjectMock(mojo, Closure<String> onTarget) {
+        def stub = new StubFor(Project)
+        def demand = stub.demand
+        demand.setProperty { prop, val ->
+            println "setting prop ${prop}"
+            propsUsed[prop] = val
+        }
+        demand.executeTarget { t ->
+            targetsRun << t
+            println "mock execute target ${t}"
+            onTarget(t)
+        }
+        demand.fireBuildStarted {}
+        demand.init {}
+        demand.fireBuildFinished { e -> }
+        mojo.metaClass.createAntProject = { ->
+            GroovySystem.getMetaClassRegistry().setMetaClass(stub.proxy.theClass, stub.proxy)
+            new Project()
+        }
+    }
+}

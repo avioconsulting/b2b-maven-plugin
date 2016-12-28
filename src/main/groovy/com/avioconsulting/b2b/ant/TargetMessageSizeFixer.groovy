d0a46697ce4dda9c02847b41235863dfd0947357
Brady Wied <brady@wied.us>
2016-12-28 11:01:27 -0600

    Add first test

--- src/main/groovy/com/avioconsulting/b2b/ant/TargetMessageSizeFixer.groovy
+++ src/main/groovy/com/avioconsulting/b2b/ant/TargetMessageSizeFixer.groovy
@@ -8,6 +8,14 @@ class TargetMessageSizeFixer {
     }
 
     void fixAntTarget(File b2bAntFilePath) {
-
+        def parser = new XmlParser()
+        Node antNode = parser.parse(b2bAntFilePath)
+        Node utilityTarget = antNode.target.find { t -> t.@name == 'utility' }
+        if (!utilityTarget.java.any()) {
+            throw new Exception('Unable to find java ANT task call!')
+        }
+        Node javaTask = utilityTarget.java[0]
+        javaTask.appendNode 'jvmarg', [value: "-Dweblogic.MaxMessageSize=40000000"]
+        new XmlNodePrinter(new IndentPrinter(new FileWriter(b2bAntFilePath))).print antNode
     }
 }
--- src/test/groovy/com/avioconsulting/b2b/ant/TargetMessageSizeFixerTest.groovy
+++ src/test/groovy/com/avioconsulting/b2b/ant/TargetMessageSizeFixerTest.groovy
@@ -1,19 +1,17 @@
 package com.avioconsulting.b2b.ant
 
-import com.avioconsulting.b2b.filenaming.Renamer
-import groovy.test.GroovyAssert
 import org.apache.commons.io.FileUtils
-import org.apache.commons.io.FilenameUtils
 import org.junit.Before
 import org.junit.Test
 
 import static junit.framework.TestCase.fail
-import static org.hamcrest.Matchers.*
-import static org.hamcrest.core.IsNot.not
+import static org.hamcrest.Matchers.equalTo
+import static org.hamcrest.Matchers.is
 import static org.junit.Assert.assertThat
 
 class TargetMessageSizeFixerTest {
     def directory = new File('build/tmp/antFix')
+    def targetFile = new File(directory, 'file.xml')
     def fixer = new TargetMessageSizeFixer({ str -> println "From class: ${str}" })
 
     @Before
@@ -21,16 +19,36 @@ class TargetMessageSizeFixerTest {
         if (directory.exists()) {
             directory.deleteDir()
         }
+        directory.mkdirs()
+    }
+
+    def getJvmArgs() {
+        def antNode = new XmlParser().parse(targetFile)
+        Node utilityTarget = antNode.target.find { t -> t.@name == 'utility' }
+        if (!utilityTarget.java.any()) {
+            throw new Exception('Unable to find java ANT task call!')
+        }
+        Node javaTask = utilityTarget.java[0]
+        javaTask.jvmarg.collect { n -> n.@value }
     }
 
     @Test
     void notThere() {
         // arrange
+        FileUtils.copyFile new File('src/test/resources/ant/noSetting.xml'), targetFile
 
         // act
+        fixer.fixAntTarget targetFile
 
         // assert
-        fail 'write this'
+        assertThat this.jvmArgs,
+                   is(equalTo(['-Xms512m',
+                               '-Xmx1024m',
+                               '-Djava.naming.provider.url=${java.naming.provider.url}',
+                               '-Djava.naming.factory.initial=${java.naming.factory.initial}',
+                               '-Djava.naming.security.principal=${java.naming.security.principal}',
+                               '-Djava.naming.security.credentials=${java.naming.security.credentials}',
+                               '-Dweblogic.MaxMessageSize=40000000']))
     }
 
     @Test

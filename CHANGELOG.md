19946159b4784d38512f17877884db5a9ec8b88c
Brady Wied <brady@wied.us>
2017-01-11 09:42:34 -0700

    Use JAR extension on packaged ZIP file
    Simplify package mojo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ## 1.0.6
 * Clean the target/classes directory automatically to avoid cruft from previous run being deployed
+* Package goal creates a file w/ JAR extension rather than ZIP to be consistent with artifacts
 
 ## 1.0.5
 * Final round of Windows locking fixes
--- src/main/groovy/com/avioconsulting/b2b/maven/B2bPackageMojo.groovy
+++ src/main/groovy/com/avioconsulting/b2b/maven/B2bPackageMojo.groovy
@@ -3,19 +3,12 @@ package com.avioconsulting.b2b.maven
 import org.apache.maven.plugin.MojoExecutionException
 import org.apache.maven.plugin.MojoFailureException
 import org.apache.maven.plugins.annotations.Mojo
-import org.apache.maven.plugins.annotations.Parameter
 
 @SuppressWarnings("GroovyUnusedDeclaration")
 @Mojo(name = 'b2bPackage')
 class B2bPackageMojo extends AbstractB2bMojo {
-    @Parameter(defaultValue = '${project.build.outputDirectory}', required = true)
-    private File classesDirectory
-    @Parameter(defaultValue = '${project.build.directory}', required = true)
-    private File outputDirectory
-    @Parameter(defaultValue = '${project.build.finalName}', readonly = true)
-    private String finalName
-
     void execute() throws MojoExecutionException, MojoFailureException {
+        def mavenBuild = this.project.build
         def antBuilder = new AntBuilder()
         this.log.info 'Arranging B2B directory structure...'
         def tmpDir = this.tempDirectory
@@ -25,9 +18,9 @@ class B2bPackageMojo extends AbstractB2bMojo {
         def expectedSoaPath = join tmpDir, 'soa'
         expectedSoaPath.mkdirs()
         antBuilder.copy(todir: expectedSoaPath) {
-            fileset(dir: join(this.classesDirectory))
+            fileset(dir: mavenBuild.outputDirectory)
         }
-        def zipFile = join this.outputDirectory, "${this.finalName}.zip"
+        def zipFile = join new File(mavenBuild.directory), "${mavenBuild.finalName}.jar"
         this.log.info "Zipping up B2B artifacts into ${zipFile}..."
         antBuilder.zip(destFile: zipFile.absolutePath) {
             fileset(dir: tmpDir)
